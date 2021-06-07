237481cad99a1e418fbe7c11aafc6c585db4cea2
Kunal <hellokunalpawar@gmail.com>
2021-06-07 21:14:51 +0530

    from start till adding crouching

---
+++ SCR_Models.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SCR_Models
+{
+	public enum PlayerStance
+	{
+		Stand,
+		Crouch,
+		Prone
+	}
+
+	[Serializable]
+    public class PlayerSettingsModel
+    {
+    	[Header("ViewSettings")]
+    	public float ViewXSensitivity;
+    	public float ViewYSensitivity;
+
+    	public bool ViewXInverted;
+    	public bool ViewYInverted;
+
+    	[Header("Movement - Running")]
+    	public float RunningForwardSpeed;
+    	public float RunningStrafeSpeed;
+
+    	[Header("Movement - Walking")]
+    	public float WalkingForwardSpeed;
+    	public float WalkingStrafeSpeed;
+    	public float WalkingBackwardSpeed;
+
+    	[Header("Jumping")]
+    	public float JumpingHeight;
+    	public float JumpingFalloff;
+    }
+
+	[Serializable]
+    public class CharacterStance
+    {
+    	public float CameraHeight;
+    	public CapsuleCollider StanceCollider;
+    }
+}
